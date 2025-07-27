17a21808b37bce36f93576a24bf85d28793a1811
sojeong0302 <01duqrl@naver.com>
2025-07-28 00:11:46 +0900

    conf: styled-components 설치 #3

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header style={{ padding: "1rem", background: "#1F3B73", color: "white" }}>
+            <h1>🚚 새마을 트럭</h1>
+        </header>
+    );
+}
