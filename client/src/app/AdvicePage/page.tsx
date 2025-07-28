65a55ce23b61380b9cfb40e88004832d6676b718
sojeong0302 <01duqrl@naver.com>
2025-07-28 14:50:01 +0900

    feat: 라우터 작업 #7

---
+++ client/src/app/AdvicePage/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function AdvicePage() {
+    return <div>상담페이지입니다.</div>;
+}
---
+++ client/src/app/ReviewPage/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ReviewPage() {
+    return <div>후기 페이지입니다.</div>;
+}
