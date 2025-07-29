51e9b87817a7f11fbce7be3f1d7da433acdff01e
sojeong0302 <01duqrl@naver.com>
2025-07-29 15:16:28 +0900

    desgin: 디자인 수정 #11

--- client/src/components/WritingDetail/index.tsx
+++ client/src/components/WritingDetail/index.tsx
@@ -19,42 +19,54 @@ export default function WritingDetail({ id }: WritingDetailProps) {
     }
     const router = useRouter();
     return (
-        <div className="w-[100%] p-10">
-            <div className="w-[100%] flex flex-col justify-center items-center">
-                <div>{post.title}</div>
+        <div className="w-[100%] p-10 flex flex-col gap-5">
+            <div className="w-[100%] flex flex-col justify-center items-center gap-10">
+                <div className="font-medium w-[100%] text-3xl border-b-2 border-[#575757] p-4">{post.title}</div>
                 {post.images && post.images.length > 0 && (
                     <>
-                        <Swiper
-                            modules={[Navigation]}
-                            navigation
-                            spaceBetween={10}
-                            slidesPerView={4}
-                            loop
-                            className="w-full"
-                        >
-                            {post.images.map((src, index) => (
-                                <SwiperSlide key={index}>
-                                    <img
-                                        src={src}
-                                        alt={`truck-${index}`}
-                                        className="w-full h-[200px] object-cover rounded shadow cursor-pointer"
-                                        onClick={() => {
-                                            setSelectedIndex(index);
-                                            setOpen(true);
-                                        }}
-                                    />
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
-                        <Lightbox
-                            open={open}
-                            close={() => setOpen(false)}
-                            index={selectedIndex}
-                            slides={post.images.map((src) => ({ src }))}
-                        />
+                        <div className="relative w-full px-20">
+                            <Swiper
+                                modules={[Navigation]}
+                                navigation={{
+                                    nextEl: ".custom-next",
+                                    prevEl: ".custom-prev",
+                                }}
+                                spaceBetween={10}
+                                slidesPerView={4}
+                                loop
+                            >
+                                {post.images.map((src, index) => (
+                                    <SwiperSlide key={index}>
+                                        <img
+                                            onClick={() => {
+                                                setSelectedIndex(index);
+                                                setOpen(true);
+                                            }}
+                                            src={src}
+                                            alt={`truck-${index}`}
+                                            className="border border-black w-full h-[200px] object-cover shadow cursor-pointer"
+                                        />
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+
+                            {/* 사진 영역 바깥에 화살표 수동 배치 */}
+                            <div className="custom-prev absolute -left-0 top-1/2 -translate-y-1/2 text-[#2E7D32] text-4xl z-10 cursor-pointer">
+                                ❮
+                            </div>
+                            <div className="custom-next absolute -right-0 top-1/2 -translate-y-1/2 text-[#2E7D32] text-4xl z-10 cursor-pointer">
+                                ❯
+                            </div>
+                            <Lightbox
+                                open={open}
+                                close={() => setOpen(false)}
+                                index={selectedIndex}
+                                slides={post.images.map((src) => ({ src }))}
+                            />
+                        </div>
                     </>
                 )}
-                <div className="">{post.content}</div>
+                <div className="text-2xl w-full bg-white border-4 border-[#2E7D32] p-4 rounded-md">{post.content}</div>
             </div>
             <div className="flex justify-end">
                 <ShortButton onClick={() => router.back()} className="bg-[#2E7D32] text-white">
