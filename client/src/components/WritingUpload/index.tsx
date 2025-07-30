a92697b09e7e237d2a4b99af96f995e30e0953bb
sojeong0302 <01duqrl@naver.com>
2025-07-31 00:10:03 +0900

    desgin: 디자인 수정 #23

--- client/src/components/WritingUpload/index.tsx
+++ client/src/components/WritingUpload/index.tsx
@@ -8,7 +8,7 @@ export default function WritingUpload() {
     };
 
     return (
-        <div className="w-[80%] h-[100%] mx-auto flex flex-col justify-center p-10 gap-7">
+        <div className="w-[80%] h-[100%] mx-auto flex flex-col justify-center p-20 gap-7">
             <input
                 placeholder="제목을 입력해 주세요."
                 className="font-medium w-full text-3xl border-b-2 border-[#575757] p-4 focus:outline-none"
