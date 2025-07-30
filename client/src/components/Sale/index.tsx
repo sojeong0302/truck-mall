5c5f26d7624a221baa1c14d2b782ceb6cd98c496
sojeong0302 <01duqrl@naver.com>
2025-07-30 21:19:33 +0900

    desgin: 페이지네이션 추가 #19

--- client/src/components/Sale/index.tsx
+++ client/src/components/Sale/index.tsx
@@ -3,8 +3,22 @@
 import { SaleProps } from "./Sale.types";
 import { SaleComponentProps } from "./Sale.types";
 import ShortButton from "@/components/ShortButton";
+import Pagination from "../Pagination";
+import { usePaginationStore } from "@/store/paginationStore";
+import { dummyData3 } from "@/data/dummy";
+
+const ITEMS_PER_PAGE = 5;
 
 export default function Sale({ posts, basePath }: SaleComponentProps) {
+    const { currentPage } = usePaginationStore();
+
+    // 현재 페이지에 맞게 데이터 자르기
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    const pagedData = dummyData3.slice(startIndex, endIndex);
+
+    const totalPages = Math.ceil(dummyData3.length / ITEMS_PER_PAGE);
+
     return (
         <div className="w-[100%] flex flex-col items-center justify-center">
             <div className="w-[70%] p-3 flex items-center gap-3  border-b-2 border-[#575757]">
@@ -12,7 +26,7 @@ export default function Sale({ posts, basePath }: SaleComponentProps) {
                 <div className="text-base">허위 없은 100% 실매물만 등록됩니다.</div>
             </div>
             <div className="w-[70%] flex flex-col gap-5 p-10 ">
-                {posts.map((post, idx) => (
+                {pagedData.map((post, idx) => (
                     <div key={idx} className="flex w-full p-3 justify-between items-center cursor-pointer">
                         {post.thumbnail && (
                             <img
@@ -30,13 +44,16 @@ export default function Sale({ posts, basePath }: SaleComponentProps) {
                         <div className="text-xl font-semibold">{post.price?.toLocaleString()}원</div>
                         <div className="text-xl font-semibold">상담문의: 010-8191-8244</div>
                         <div>
-                            <ShortButton className={`text-white ${post.status ? "bg-[#2E7D32]" : "bg-[#C62828]"}`}>
+                            <div
+                                className={`flex justify-center shadow-lg text-2xl  w-[120px]  p-2.5 rounded-md font-medium text-white ${post.status ? "bg-[#2E7D32]" : "bg-[#C62828]"}`}
+                            >
                                 {post.status ? "판매중" : "판매완료"}
-                            </ShortButton>
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>
+            <Pagination totalPages={totalPages} />
         </div>
     );
 }
