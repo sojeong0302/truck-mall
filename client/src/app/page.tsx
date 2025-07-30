d86f06a6e40e2e98e4408d70842db426b192842d
sojeong0302 <01duqrl@naver.com>
2025-07-30 15:41:04 +0900

    desgin: tip 초기 디자인 #19

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -32,10 +32,23 @@ export default function MainPage() {
                 </Swiper>
             </div>
 
-            <div className="flex w-[80%] justify-center">
+            <div className="flex w-[100%] gap-10 p-25">
                 <Sns />
-                <div>중간 오른쪽</div>
+                {/* TIP 바운딩 박스 */}
+                <div className="relative w-full p-6 border-5 border-[#2E7D32] rounded-4xl bg-[#F5F5F5]">
+                    {/* 중앙 TIP 텍스트 */}
+                    <div className="absolute -top-4 left-1/2 -translate-x-1/2 px-4 bg-[#F5F5F5] z-10 text-[#D4B76A] text-4xl font-bold">
+                        TIP
+                    </div>
+
+                    {/* TIP 콘텐츠 영역 */}
+                    <div className="mt-4 text-center">
+                        <p className="font-semibold text-xl mb-2">여름철 트럭 관리 요령 및 체크사항</p>
+                        <p className="text-gray-500">이제 슬슬 날씨가 본격적인 하절기 시작에 접어드는 것 같습니다...</p>
+                    </div>
+                </div>
             </div>
+
             <div className="">매물</div>
         </div>
     );
