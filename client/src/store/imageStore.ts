b3e9162f91d7c47c2b3dff0945d932ded6bc7f9b
sojeong0302 <01duqrl@naver.com>
2025-07-31 21:04:16 +0900

    feat: etcpto 가져오기 #25

--- client/src/store/imageStore.ts
+++ client/src/store/imageStore.ts
@@ -3,15 +3,17 @@ import { create } from "zustand";
 
 interface ImageStore {
     previews: string[];
-    addPreview: (image: string) => void;
+    addPreview: (img: string) => void;
     removePreview: (index: number) => void;
+    setPreviews: (imgs: string[]) => void;
 }
 
 export const useImageStore = create<ImageStore>((set) => ({
     previews: [],
-    addPreview: (image) => set((state) => ({ previews: [...state.previews, image] })),
+    addPreview: (img) => set((state) => ({ previews: [...state.previews, img] })),
     removePreview: (index) =>
         set((state) => ({
             previews: state.previews.filter((_, i) => i !== index),
         })),
+    setPreviews: (imgs) => set({ previews: imgs }),
 }));
