cee986df408c6c71f745f88b30ce1835352e7c03
sojeong0302 <01duqrl@naver.com>
2025-07-31 22:39:48 +0900

    desgin: 매물 상세 페이지 #29

--- client/src/app/SaleDetailPage/[id]/page.tsx
+++ client/src/app/SaleDetailPage/[id]/page.tsx
@@ -2,10 +2,65 @@
 
 import { use } from "react";
 import { dummyData3 } from "@/data/dummy";
+import SwiperWithLightbox from "@/components/SwiperWithLightbox";
+import ShortButton from "@/components/ShortButton";
+import { useRouter } from "next/navigation";
 
 export default function ReviewPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = use(params);
     const post = dummyData3.find((item) => item.id === Number(id));
+    const router = useRouter();
+    if (!post) {
+        return <div className="p-10 text-red-500">해당 게시물을 찾을 수 없습니다.</div>;
+    }
 
-    return <>매물 상세 페이지입니다.</>;
+    return (
+        <div className="w-full flex justify-center flex-col items-center p-15">
+            <div className="w-[80%] flex flex-col gap-15">
+                <div className="w-full flex justify-center gap-15">
+                    <img src={post.thumbnail} className="border-1 shadow-lg rounded-xl w-[50%] h-auto min-w-[150px]" />
+                    <div className="flex flex-col justify-around">
+                        <div className="font-bold text-4xl border-b-2 border-[#575757] p-2">{post.name}</div>
+                        <div className="flex flex-col text-2xl p-2 gap-5">
+                            <div className="flex gap-3">
+                                <div className="font-bold">연료:</div>
+                                <div>{post.fuel}</div>
+                            </div>
+                            <div className="flex gap-3">
+                                <div className="font-bold">차체 타입:</div>
+                                <div>{post.type}</div>
+                            </div>
+                            <div className="flex gap-3">
+                                <div className="font-bold">트림:</div>
+                                <div>{post.trim}</div>
+                            </div>
+                            <div className="flex gap-3">
+                                <div className="font-bold">연식:</div>
+                                <div>{post.year}</div>
+                            </div>
+                            <div className="flex gap-3">
+                                <div className="font-bold">주행거리:</div>
+                                <div>{post.mileage}</div>
+                            </div>
+                            <div className="flex gap-3">
+                                <div className="font-bold">색상:</div>
+                                <div>{post.color}</div>
+                            </div>
+                            <div className="flex gap-3">
+                                <div className="font-bold">가격:</div>
+                                <div className="text-[#C62828]">{post.price}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {post.images && post.images.length > 0 && <SwiperWithLightbox images={post.images} />}
+                <div className="text-2xl w-full bg-white border-4 border-[#2E7D32] p-4 rounded-md">{post.content}</div>
+                <div className="flex justify-end">
+                    <ShortButton onClick={() => router.back()} className="bg-[#2E7D32] text-white">
+                        뒤로가기
+                    </ShortButton>
+                </div>
+            </div>
+        </div>
+    );
 }
--- client/src/components/WritingDetail/index.tsx
+++ client/src/components/WritingDetail/index.tsx
@@ -1,18 +1,13 @@
 "use client";
-import { WritingDetailProps } from "./WritingDetail.types";
-import { dummyData } from "@/data/dummy";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "yet-another-react-lightbox/styles.css";
-import Lightbox from "yet-another-react-lightbox";
-import { useLightboxStore } from "@/store/lightboxStore";
 import ShortButton from "@/components/ShortButton";
 import { useRouter } from "next/navigation";
+import SwiperWithLightbox from "@/components/SwiperWithLightbox";
 
 export default function WritingDetail({ post }: { post: any }) {
-    const { open, selectedIndex, setOpen, setSelectedIndex } = useLightboxStore();
     if (!post) {
         return <div className="p-10 text-red-600">해당 글을 찾을 수 없습니다.</div>;
     }
@@ -21,50 +16,7 @@ export default function WritingDetail({ post }: { post: any }) {
         <div className="w-[100%] p-10 flex flex-col gap-5 items-center">
             <div className="w-[80%] flex flex-col justify-center items-center gap-10">
                 <div className="font-medium w-[100%] text-3xl border-b-2 border-[#575757] p-4">{post.title}</div>
-                {post.images && post.images.length > 0 && (
-                    <>
-                        <div className="relative w-full px-20">
-                            <Swiper
-                                modules={[Navigation]}
-                                navigation={{
-                                    nextEl: ".custom-next",
-                                    prevEl: ".custom-prev",
-                                }}
-                                spaceBetween={10}
-                                slidesPerView={4}
-                                loop
-                            >
-                                {post.images.map((src: string, index: number) => (
-                                    <SwiperSlide key={index}>
-                                        <img
-                                            onClick={() => {
-                                                setSelectedIndex(index);
-                                                setOpen(true);
-                                            }}
-                                            src={src}
-                                            alt={`truck-${index}`}
-                                            className="w-full h-[200px] object-cover shadow cursor-pointer border border-black"
-                                        />
-                                    </SwiperSlide>
-                                ))}
-                            </Swiper>
-
-                            {/* 사진 영역 바깥에 화살표 수동 배치 */}
-                            <div className="custom-prev absolute -left-0 top-1/2 -translate-y-1/2 text-[#2E7D32] text-4xl z-10 cursor-pointer transition transform duration-200 hover:scale-110 active:scale-95">
-                                ❮
-                            </div>
-                            <div className="custom-next absolute -right-0 top-1/2 -translate-y-1/2 text-[#2E7D32] text-4xl z-10 cursor-pointer transition transform duration-200 hover:scale-110 active:scale-95">
-                                ❯
-                            </div>
-                            <Lightbox
-                                open={open}
-                                close={() => setOpen(false)}
-                                index={selectedIndex}
-                                slides={post.images.map((src: string) => ({ src }))}
-                            />
-                        </div>
-                    </>
-                )}
+                {post.images && post.images.length > 0 && <SwiperWithLightbox images={post.images} />}
                 <div className="text-2xl w-full bg-white border-4 border-[#2E7D32] p-4 rounded-md">{post.content}</div>
             </div>
             <div className="flex justify-end w-[80%]">
