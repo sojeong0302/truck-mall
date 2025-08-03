15ff31e264095faff214787aa43978d508c2f715
sojeong0302 <01duqrl@naver.com>
2025-08-03 16:26:17 +0900

    feat:삭제 api 연동 #49

--- client/src/components/WritingDetail/index.tsx
+++ client/src/components/WritingDetail/index.tsx
@@ -7,17 +7,43 @@ import ShortButton from "@/components/ShortButton";
 import { useRouter } from "next/navigation";
 import SwiperWithLightbox from "@/components/SwiperWithLightbox";
 import { useAuthStore } from "@/store/useAuthStore";
+import Modal from "../Modal";
+import { useModalStore } from "@/store/ModalStateStroe";
 
-export default function WritingDetail({ id, post, crystalPath }: { id: string; post: any; crystalPath: string }) {
+export default function WritingDetail({
+    id,
+    post,
+    crystalPath,
+    url,
+}: {
+    id: string;
+    post: any;
+    crystalPath: string;
+    url: string;
+}) {
     if (!post) {
         return <div className="p-10 text-red-600">해당 글을 찾을 수 없습니다.</div>;
     }
+    const store = useModalStore();
+    const { isModalOpen, setIsModalOpen } = store;
 
     const router = useRouter();
     const { isLoggedIn } = useAuthStore();
 
     const handleGoCrystal = () => {
         router.push(`/${crystalPath}/${id}`);
+    };
+
+    const handleDelete = async () => {
+        try {
+            const res = await fetch(`http://localhost:5000/review/${id}`, {
+                method: "DELETE",
+            });
+            setIsModalOpen(true);
+        } catch (error) {
+            console.error("삭제 오류", error);
+            alert("서버 오류로 삭제에 실패했습니다.");
+        }
     };
 
     return (
@@ -27,7 +53,10 @@ export default function WritingDetail({ id, post, crystalPath }: { id: string; p
                     <div onClick={handleGoCrystal} className="cursor-pointer">
                         수정
                     </div>
-                    |<div className="cursor-pointer">삭제</div>
+                    |
+                    <div className="cursor-pointer" onClick={handleDelete}>
+                        삭제
+                    </div>
                 </div>
             )}
             <div className="w-[80%] flex flex-col justify-center items-center gap-10">
@@ -40,6 +69,7 @@ export default function WritingDetail({ id, post, crystalPath }: { id: string; p
                     뒤로가기
                 </ShortButton>
             </div>
+            {isModalOpen && <Modal url={url} text={"삭제된 내용은 복구할 수 없습니다.\n정말 삭제하시겠습니까?"} />}
         </div>
     );
 }
