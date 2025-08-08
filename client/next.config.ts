169cbdca08e0ff331943ced8d5ad9c765e4597cf
sojeong0302 <01duqrl@naver.com>
2025-08-08 14:17:51 +0900

    cpmmit

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,10 @@
+// client/next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         domains: ["localhost"], // 여기에 Flask 서버 도메인 추가
     },
