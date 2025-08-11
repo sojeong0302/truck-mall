e1f301371db2bcf598ee3612e1b93c31e5c1687f
sojeong0302 <01duqrl@naver.com>
2025-08-12 00:03:59 +0900

    commit

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true, // ✅ 빌드 단계에서 ESLint 무시
+    },
     images: {
+        // 절대경로 이미지 표시 위해 백엔드 호스트 허용
         domains: ["www.saemaeultruck.pics"],
+        // 또는 remotePatterns 사용 가능
+        // remotePatterns: [{ protocol: "https", hostname: "www.saemaeultruck.pics" }],
     },
 };
 
