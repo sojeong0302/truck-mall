5bd98648cfefede4157b0b7bce019d0d1e610d5e
sojeong0302 <01duqrl@naver.com>
2025-08-12 01:14:51 +0900

    commit

--- client/src/utils/getImageUrl.ts
+++ client/src/utils/getImageUrl.ts
@@ -1,6 +1,8 @@
 // utils/getImageUrl.ts
 export const getImageUrl = (path: string) => {
     if (!path) return "";
-    if (path.startsWith("http")) return path;
-    return `${process.env.NEXT_PUBLIC_API_URL}${path}`;
+    if (path.startsWith("http")) return path; // 이미 절대 경로면 그대로 반환
+
+    // ✅ 백엔드 URL + 상대 경로
+    return `https://www.saemaeultruck.pics${path}`;
 };
