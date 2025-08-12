76459fe39e2b80fb138e9307393d7b6e765353dd
sojeong0302 <01duqrl@naver.com>
2025-08-12 21:22:35 +0900

    commit

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,3 +1,26 @@
+// import "./globals.css";
+// import Header from "../components/Header";
+// import Footer from "../components/Footer";
+// import AppInitializer from "@/components/AppInitializer";
+
+// export const metadata = {
+//     title: "새마을 트럭",
+//     description: "특장차 매매 사이트",
+// };
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//     return (
+//         <html lang="ko">
+//             <AppInitializer />
+//             <body className="bg-[#F5F5F5]">
+//                 <Header />
+//                 <div className="w-[100%] mx-auto">{children}</div>
+//                 <Footer />
+//             </body>
+//         </html>
+//     );
+// }
+// app/layout.tsx
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -11,8 +34,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ko">
-            <AppInitializer />
             <body className="bg-[#F5F5F5]">
+                {/* ✅ 반드시 body 안에서 마운트 */}
+                <AppInitializer />
                 <Header />
                 <div className="w-[100%] mx-auto">{children}</div>
                 <Footer />
