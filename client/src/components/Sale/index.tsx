0880d8b53f8fd8773288bb0d75108ea1614d2810
sojeong0302 <01duqrl@naver.com>
2025-08-13 22:24:15 +0900

    Update index.tsx

--- client/src/components/Sale/index.tsx
+++ client/src/components/Sale/index.tsx
@@ -146,7 +146,7 @@ export default function Sale({ transmission, posts, priceRange, yearRange }: Sal
                                         className="w-full h-full object-cover"
                                     />
                                 ) : (
-                                    <div className="text-gray-500 flex items-center justify-center text-sm text-center">
+                                    <div className="flex w-full h-full items-center justify-center text-sm text-center text-gray-500">
                                         이미지 준비중 입니다.
                                     </div>
                                 )}
