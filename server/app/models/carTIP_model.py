385760eae851cfeacb95d3625f24f1b8c65216bc
sojeong0302 <01duqrl@naver.com>
2025-08-13 12:14:22 +0900

    commit

--- server/app/models/carTIP_model.py
+++ server/app/models/carTIP_model.py
@@ -1,11 +1,12 @@
 from app.extensions import db
 from sqlalchemy.dialects.sqlite import JSON
+from sqlalchemy.sql import func
 
 
 class CarTIP(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    images = db.Column(JSON)  # 리스트 형태로 저장
+    images = db.Column(JSON)
     content = db.Column(db.Text, nullable=False)
-    date = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, server_default=func.now(), nullable=False)
     view = db.Column(db.Integer, default=0)
--- server/app/models/review_model.py
+++ server/app/models/review_model.py
@@ -5,7 +5,7 @@ from sqlalchemy.dialects.sqlite import JSON
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    images = db.Column(JSON)  # 리스트 형태로 저장
+    images = db.Column(JSON)
     content = db.Column(db.Text, nullable=False)
     date = db.Column(db.String(100), nullable=False)
     view = db.Column(db.Integer, default=0)
