a5cf7b57f0fed9c4ef435a3a4c8933bd71019436
sojeong0302 <01duqrl@naver.com>
2025-08-15 15:06:36 +0900

    Update page.tsx

--- client/src/app/SaleDetailPage/[id]/page.tsx
+++ client/src/app/SaleDetailPage/[id]/page.tsx
@@ -158,7 +158,7 @@ export default function ReviewPage({ params }: { params: Promise<{ id: string }>
                     <SwiperWithLightbox images={post.images.map((img) => getImageUrl(img))} />
                 )}
 
-                <div className="text-xl sm:text-2xl w-full bg-white border-4 border-[#2E7D32] p-4 rounded-md">
+                <div className="text-xl h-[80px] sm:text-2xl w-full bg-white border-4 border-[#2E7D32] p-4 rounded-md">
                     {post.content}
                 </div>
                 <div className="flex justify-end gap-3">
