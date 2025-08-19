a7fafb87a22807b940eee2a606e5bc01cf9c3a9c
sojeong0302 <01duqrl@naver.com>
2025-08-20 01:19:46 +0900

    Update index.tsx

--- client/src/components/Sale/index.tsx
+++ client/src/components/Sale/index.tsx
@@ -151,9 +151,9 @@ export default function Sale({ transmission, priceRange, yearRange }: SaleCompon
                                     </div>
                                 )}
                             </div>
-                            <div className="max-w-[110px] sm:max-w-[350px] fade-truncate hidden sm:block text-sm sm:text-2xl font-semibold flex flex-col gap-3">
+                            <div className="fade-truncate hidden sm:block text-sm sm:text-2xl font-semibold flex flex-col gap-3">
                                 <div className="">{post.name}</div>
-                                <div className="flex">
+                                <div className="flex gap">
                                     <div className="">{post.year}</div>
                                     <div className="">{post.mileage}</div>
                                     <div className="">{post.fuel}</div>
