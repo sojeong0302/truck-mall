2fc6e75b08115333d2e18f90a67466184a27eb06
sojeong0302 <01duqrl@naver.com>
2025-08-20 13:31:11 +0900

    Update sale_model.py

--- server/app/models/sale_model.py
+++ server/app/models/sale_model.py
@@ -8,18 +8,19 @@ class Sale(db.Model):
     __tablename__ = "sale"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)  # 이름 0
-    fuel = db.Column(db.String(50))  # 연료 0
-    type = db.Column(db.String(50))  # 타입
-    trim = db.Column(db.String(50))  # 트림
-    year = db.Column(db.Integer)  # 연식
-    mileage = db.Column(db.String(50))  # 주행 거리 0
-    color = db.Column(db.String(30))  # 색상 0
-    price = db.Column(db.Integer)  # 가격
+    name = db.Column(db.String(100), nullable=False)  # 이름
     car_number = db.Column(db.String(50))  # 차량번호
+    price = db.Column(db.Integer)  # 가격
+    year = db.Column(db.Integer)  # 연식
+    fuel = db.Column(db.String(50))  # 연료
+    transmission = db.Column(db.String(20))  # 변속기
+    color = db.Column(db.String(30))  # 색상
+    mileage = db.Column(db.Integer)  # 주행 거리
     vin = db.Column(db.String(50))  # 차대번호
-    accident_info = db.Column(db.String(50))  # 사고정보
-    combination_info = db.Column(db.String(50))  # 조합정보
+
+    # 추가
+    suggest_number = db.Column(db.String(50))  # 제시 번호
+    performance_number = db.Column(db.String(50))  # 성능 번호
 
     # Filter
     manufacturer = db.Column(db.String(50))
@@ -27,7 +28,9 @@ class Sale(db.Model):
     sub_model = db.Column(db.String(50))
     grade = db.Column(db.String(50))
 
-    transmission = db.Column(db.String(20))  # 변속기 0
+    # Filter 계층형 구조
+    normal_tags = db.Column(JSON)
+
     thumbnail = db.Column(db.String(255))  # 썸네일
     content = db.Column(db.Text)  # 내용
     simple_content = db.Column(db.String(100))  # 간단 내용
@@ -35,35 +38,30 @@ class Sale(db.Model):
     status = db.Column(Boolean, default=True, nullable=False)  # 판매 상태
     simple_tags = db.Column(JSON)  # SimpleFilter
 
-    # Filter 계층형 구조
-    normal_tags = db.Column(JSON)
-
     # json 형태로 변환
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
-            "fuel": self.fuel,
-            "type": self.type,
-            "trim": self.trim,
-            "year": self.year,
-            "mileage": self.mileage,
-            "color": self.color,
+            "car_number": self.car_number,
             "price": self.price,
+            "year": self.year,
+            "fuel": self.fuel,
+            "transmission": self.transmission,
+            "color": self.color,
+            "mileage": self.mileage,
+            "vin": self.vin,
+            "performance_number": self.performance_number,
+            "suggest_number": self.suggest_number,
             "manufacturer": self.manufacturer,
             "model": self.model,
             "sub_model": self.sub_model,
             "grade": self.grade,
-            "transmission": self.transmission,
+            "normal_tags": self.normal_tags,
             "thumbnail": self.thumbnail,
             "content": self.content,
             "simple_content": self.simple_content,
-            "status": self.status,
             "images": self.images,
+            "status": self.status,
             "simple_tags": self.simple_tags,
-            "normal_tags": self.normal_tags,
-            "car_number": self.car_number,
-            "vin": self.vin,
-            "accident_info": self.accident_info,
-            "combination_info": self.combination_info,
         }
