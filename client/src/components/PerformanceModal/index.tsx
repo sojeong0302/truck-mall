3d97c80dab0ac673b17dd57f1db3d42aea2612f5
sojeong0302 <01duqrl@naver.com>
2025-08-27 04:07:43 +0900

    Update index.tsx

--- client/src/components/PerformanceModal/index.tsx
+++ client/src/components/PerformanceModal/index.tsx
@@ -1,19 +1,9 @@
 "use client";
 
 export default function PerformanceModal() {
-    console.log("[PerformanceModal] render");
     return (
-        <div
-            style={{
-                position: "fixed",
-                inset: 0,
-                display: "grid",
-                placeItems: "center",
-                background: "rgba(0,0,0,.4)",
-                zIndex: 9999,
-            }}
-        >
-            <div style={{ background: "#fff", padding: 20, borderRadius: 12 }}>모달입니당</div>
+        <div className="w-[80%] h-[80%] flex justify-center items-center">
+            <div className="flex justify-center items-center">모달입니당</div>
         </div>
     );
 }
